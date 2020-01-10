97f17e4792d1da8679721ce3796a9d5acfb9abe0
dewaqintoro <dewaqintoro@gmail.com>
2020-01-11 00:18:12 +0700

    v2

--- dewCompVis.php
+++ dewCompVis.php
@@ -28,7 +28,7 @@ if (isset($_POST['submit'])) {
 <div class="container">
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-		<a class="navbar-brand" href="#">DewAzure</a>
+		<a class="navbar-brand" href="index.php">DewAzure</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
